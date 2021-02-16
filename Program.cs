db926f63ecd0fcb1a6bb1dbc488e9e778a539e06
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-16 10:55:22 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LesVariablesExo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Monsieur";
+            string s2 = " Vincent";
+            string s3 = string.Concat(s1, s2);
+            int number1 = 1;
+
+            Console.WriteLine("Bonjour " + s3 + ", vous êtes venu nous rendre visite " + number1 + " fois");
+
+        }
+    }
+}
