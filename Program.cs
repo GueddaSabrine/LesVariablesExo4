6a1c10125397cae178d0fefe7a543e4db02309f3
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-21 12:35:03 +0100

    Vexo4

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ namespace LesVariablesExo4
 {
     class Program
     {
+        /*Créer trois variables contenant respectivement les éléments suivants :
+    Monsieur
+    Vincent
+    1
+ En utilisant les variables, faire en sorte que la console affiche le message suivant :
+ « Bonjour Monsieur Vincent, vous êtes venu nous rendre visite 1 fois »*/
+
         static void Main(string[] args)
         {
             string s1 = "Monsieur";
@@ -11,7 +18,7 @@ namespace LesVariablesExo4
             string s3 = string.Concat(s1, s2);
             int number1 = 1;
 
-            Console.WriteLine("Bonjour " + s3 + ", vous êtes venu nous rendre visite " + number1 + " fois");
+            Console.WriteLine("Bonjour " + s1 + " " +s2  + ", vous êtes venu nous rendre visite " + number1 + " fois");
 
         }
     }
